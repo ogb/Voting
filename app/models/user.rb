b1db6ab0bc345cd1388e4405e35f49d841cf1d8a
Alec Embke <aembke@uoregon.edu>
2013-02-01 22:50:35 -0800

    updated layouts

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ActiveRecord::Base
   
   validates :email, presence: true, format: { with: EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
+  
   before_save { |user| user.email = email.downcase }
 
 
