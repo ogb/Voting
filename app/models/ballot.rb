cac763e95777f409c8b8133061909d8befec4ab6
Alec Embke <ajembke345@gmail.com>
2013-02-02 20:43:55 -0800

    Update app/models/ballot.rb

--- app/models/ballot.rb
+++ app/models/ballot.rb
@@ -1,9 +1,8 @@
-
 require "lib/VotingAlgorithms"
 
 class Ballot < ActiveRecord::Base
   
-  has_many_and_belongs_to :user
+  has_and_belongs_to_many :user
   has_many :votes
 
   attr_accessible :algorithm, :description, :end_time, :title, :type
