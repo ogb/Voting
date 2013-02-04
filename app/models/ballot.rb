a27b63006987c73e217a9fefe03aa93feb734f45
Alec Embke <aembke@uoregon.edu>
2013-02-03 18:35:27 -0800

    added serializations for ballot inputs

--- app/models/ballot.rb
+++ app/models/ballot.rb
@@ -5,6 +5,9 @@ class Ballot < ActiveRecord::Base
   has_and_belongs_to_many :users
   has_many :candidates, dependent: :destroy
   has_many :votes, through: :candidates
+  # this is ugly... but it avoids nested models in nested forms. we don't have time for that on the first iteration
+  serialize :voters_input
+  serialize :candidates_input
 
   attr_accessible :algorithm, :description, :end_time, :title, :type, :public_results, :candidates_input, :voters_input
   
